b0e8f24718b201045fbbb7c432ac87caa36061e4
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2017-01-30 22:12:00 -0800

    Creating module and opening file: module pattern

---
+++ command-line-nodejs-simple-program-2.js
@@ -0,0 +1,26 @@
+function printHelp(){
+  console.log("1.js (c) Kyle Simpson Workshop");
+  console.log("");
+  console.log("usage:");
+  console.log("--help         print this help");
+  console.log("--file={NAME}        read the file of {NAME} and output it");
+  console.log("");
+}
+
+// install module: npm install minimist
+var args = require("minimist")(process.argv.slice(2),{ string: "file"});
+
+if (args.help || !args.file){
+  printHelp();
+  process.exit(1);
+}
+
+var hello = require("./helloworld.js");
+
+var contents = hello.say(args.file);
+console.log(contents.toString());
+
+// Type node command-line-nodejs-simple-program-2.js --file=text.txt in order to launch it
+
+// NodeJSKyleSimpson [task-2] :> node command-line-nodejs-simple-program-2.js --file=text.txt
+// This is a text file. We want to test the program that should be opening this file. If you see this - Great! It worked. Time to take a break.
